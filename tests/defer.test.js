049b0ed9ed7bba42e47605068045e33a68e4972d
Sunil Pai <threepointone@gmail.com>
2012-11-23 01:51:58 +0530

    a better deferred test

--- tests/defer.test.js
+++ tests/defer.test.js
@@ -1,18 +1,43 @@
 "use strict";
 var _ = require('underscore'),
-    k = _(process.argv).indexOf('html-cov') >= 0 ? require('../lib-cov/k') : require('../lib/k'),
-    async = require('async'),
-    __should = require('should'),
-    glob = require('glob-whatev');
+	D = _(process.argv).indexOf('html-cov') >= 0 ? require('../lib-cov/defer') : require('../lib/defer'),
+	async = require('async'),
+	__should = require('should');
 
 
-describe('D', function(){
-	it('should be able to chain, and execute in order', function(done){
-		var x = k();
-		x.D.wait(200).wait(200).wait(200).then(done);
+
+describe('D', function() {
+	beforeEach(function() {
+		this.x = {
+			a: function(cb) {
+				this.b(cb);
+			},
+			b: function(cb) {
+				cb();
+			},
+			c: function(cb) {
+				cb();
+			}
+		};
+		D.deferMethod('a');
+		D.deferMethod('c');
+
+		this.x.D = new D({
+			host: this.x
+		});
 	});
+	describe('import', function() {
+		it('should import a method as a deferred version');
+		it('should import a regular method, that can still be chained');
+		it('should be able to chain, and execute in order', function(done) {
+			this.x.D.a().c().then(done);
+		});
+	});
+
+
+
 });
 
 // make a new deferred
 // import a method 
-// import an async method  
+// import an async method
