2e8f247cb2e01421e92df94f66f3370df147e195
Sunil Pai <threepointone@gmail.com>
2012-11-23 01:13:18 +0530

    deferreds first cut

--- script.js
+++ script.js
@@ -9,23 +9,36 @@ var x = k({
     base: './',
     dest: 'build'
 });
+var y;
 
-x.read(function() {
-    var y = this.clone();
-
-    this.filter(/.js$/, function() {
-        this.compress({}, function() {
-            this.hashify(function() {
-                this.write(function() {
+x.read(function(){
+    y = x.clone();
+    x.D.filter(/.js$/).compress({}).hashify().write().then(function(){
                     console.log('x', _.pluck(x.files, 'src'));
-                    console.log('y', _.pluck(y.files, 'src')); 
-                    console.log(x === y);
-                });
-            });
-        });
+                    console.log('y', _.pluck(y.files, 'src'));
+                    console.log(x === y);        
     });
 });
 
+// x.read(function() {
+//     var y = this.clone();
 
-// then, you can use deferreds and do this - 
-// k().read().filter(/.js/).compress({}).write();
+//     this.filter(/.js$/, function() {
+//         this.compress({}, function() {
+//             this.hashify(function() {
+//                 this.write(function() {
+//                     // console.log('x', _.pluck(x.files, 'src'));
+//                     // console.log('y', _.pluck(y.files, 'src')); 
+//                     // console.log(x === y);
+//                 });
+//             });
+//         });
+//     });
+// });
+
+
+x.D.wait(1500).wait(2000).read().write().then(function(){
+    console.log('done');
+});
+
+
