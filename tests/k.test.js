56ace3add2a50bbc8dfb518538196f1ef4848ee5
Sunil Pai <threepointone@gmail.com>
2012-11-25 23:04:25 +0530

    update some tests

--- tests/k.test.js
+++ tests/k.test.js
@@ -3,6 +3,8 @@
 var _ = require('underscore'),
     k = _(process.argv).indexOf('html-cov') >= 0 ? require('../lib-cov/k') : require('../lib/k'),
     __should = require('should'),
+    path = require('path'),
+    fs = require('fs'),
     glob = require('glob-whatev');
 
 describe('k', function() {
@@ -24,7 +26,9 @@ describe('k', function() {
 
         it('should resolve all file names in the beginning', function() {
             var x = k();
-            _.isEqual(_(x.files).pluck('src'), glob.glob('**/*.*')).should.be.ok;
+            _.isEqual(_(x.files).pluck('src'), _(glob.glob('**/*')).filter(function(f) {
+                return fs.statSync(f).isFile();
+            })).should.be.ok;
 
         });
     });
@@ -48,28 +52,48 @@ describe('k', function() {
             _.isFunction(x.abc).should.be.ok;
         });
 
-        it('should fail on a null fn');
-        it('should execute a task in its own scope');
-        it('should accept an object hash');
+        it('should fail on a null fn', function(){
+            (function(){
+                k.task('alpha');
+            }).should.throwError();
+        });
+
+        it('should execute a task in its own scope', function(done){
+            var x = k();
+            k.task('a', function(){
+                (x === this).should.be.ok;
+                done();
+            });
+
+            x.a();
+        });
+
+        it('should accept an object hash', function(){
+
+        });
     });
 
     describe('::clone', function() {
         it('should have equivalent config', function() {
-            var x = k({falafel:'shawarma'});
+            var x = k({
+                falafel: 'shawarma'
+            });
             var y = x.clone();
             _.isEqual(x.config, y.config).should.be.ok;
-            (x===y).should.not.be.ok;
+            (x === y).should.not.be.ok;
         });
 
         it('should have equivalent file list', function() {
-            var x = k({falafel:'shawarma'});
+            var x = k({
+                falafel: 'shawarma'
+            });
             var y = x.clone();
             _.isEqual(x.files, y.files).should.be.ok;
-            (x===y).should.not.be.ok;
+            (x === y).should.not.be.ok;
         });
     });
 
-    describe ('::chain', function(){
+    describe('::chain', function() {
         it('should clone self, start a new chain');
         it('should exec async tasks in order');
     });
