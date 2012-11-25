e47020c70fc36db960da50113f83ccba8639f1b5
Sunil Pai <threepointone@gmail.com>
2012-11-25 16:51:08 +0530

    update example to use .chain()

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ var y;
 
 x.read(function() {
     y = x.clone();
-    x.D.filter(/.js$/).compress({}).hashify().write().then(function() {
+    x.chain().filter(/.js$/).compress({}).hashify().then(function() {
         console.log('x', _.pluck(x.files, 'src'));
         console.log('y', _.pluck(y.files, 'src'));
         console.log(x === y);
     });
 });
 
-x.D.wait(1500).log('waited').wait(2000).read().write().then(function() {
+x.chain().wait(1500).log('waited').wait(2000).read().then(function() {
     console.log('done');
 });
 
@@ -31,15 +31,15 @@ k.task('a', function(done){ done(); });
 k.task('b', function(done){ done(); });
 
 k.task('c', function(done){
-    this.D.a().b().then(done);
+    this.chain().a().b().then(done);
 });
 
-k().D.c().then(function(){
+k().chain().c().then(function(){
     console.log('done!');
 });
 
-k().D.cmd('mkdir x').then(function(err, list){
-    console.log(err, list);
+k().chain().cmd('ls -al').then(function(err, list){
+    console.log(list);
 });
 
 
