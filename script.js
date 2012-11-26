30ee7375a42c8fe68fce5f168efff1c7236009ca
Sunil Pai <threepointone@gmail.com>
2012-11-26 10:00:55 +0530

    .hashify, .compress shouldn't write automatically

--- script.js
+++ script.js
@@ -8,36 +8,12 @@ var k = require('./index'),
 var x = k({
     base: './',
     dest: 'build'
-});
-var y;
+}).chain();
 
-x.read(function() {
-    y = x.clone();
-    x.chain().filter(/.js$/).compress().hashify().then(function() {
-        console.log('x', _.pluck(x.files, 'src'));
-        console.log('y', _.pluck(y.files, 'src'));
-        console.log(x === y);
-    });
-});
-
-x.chain().wait(1500).log('waited').wait(2000).read().then(function() {
-    console.log('done');
-});
-
-
-//compound tasks
-k.task('a', function(done){ done(); });
-
-k.task('b', function(done){ done(); });
-
-k.task('c', function(done){
-    this.chain().a().b().then(done);
-});
-
-k().chain().c().then(function(){
-    console.log('done!');
-});
-
-k().chain().cmd('ls -al').then(function(err, list){
-    console.log(list);
-});
+x.read()
+	.compress()
+	.hashify()
+	.write()
+	.meta()
+	.clean()
+	.log('done');
