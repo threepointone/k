404d0070661157f4b56f7c1b33b3c68e8f5a3db7
Sunil Pai <threepointone@gmail.com>
2012-12-01 16:21:17 +0530

    - exec arbit functions in the chain. sweet. - proper .js regex

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ var k = require('./index');
 k({ src: 'lib' })
 .chain()
 .clean()
-.filter(/.js$/)
+.filter(/\.js$/)
 .read()
 .compress().write().meta('compressed')
+.log('somewhere in the middle')
 .hashify().write().meta('hashed')
 .concat('built.js')
-.log('done')
+.fn(function(done){
+	console.log('exec arbit code');
+	done();
+})
+.log('just about finishing...')
+.clean()
 .then(function(){
 	console.log('done')
-}, function(){
-	console.log('failed');
+}, function(e){
+	console.log('failed', e.stack);
+
 });
 
 
 k({src:'examples/yui', dest: 'examples/dist'}).chain()
 .clean()
-.filter(/.js$/)
+.filter(/\.js$/)
 .read()
 .yuianalyze()
 .log('analyzed');
